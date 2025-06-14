54aeaa8ec78f6ae609bae5fa5768f31c4c3a495d
TheTOXIN <thetoksin@gmail.com>
2025-06-14 12:35:08 +0300

    task 38

---
+++ src/test/java/Test38.java
@@ -0,0 +1,21 @@
+import org.example.Task38;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class Test38 {
+
+    Task38 task = Mockito.spy(Task38.class);
+
+    @Test
+    public void test_1() {
+        String actual = task.countAndSay(4);
+        Assertions.assertEquals("1211", actual);
+    }
+
+    @Test
+    public void test_2() {
+        String actual = task.countAndSay(1);
+        Assertions.assertEquals("1", actual);
+    }
+}
